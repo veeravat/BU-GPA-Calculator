aa486d3f31e19e10d286f6d6c4f319f43d488c84
DESKTOP-7E8OO69\Netdesign-ND <tanut.leel@bumail.net>
2018-10-02 09:06:07 +0700

    - Remove Line

--- main.java
+++ main.java
@@ -9,17 +9,12 @@ public class main
         CipherFile decryptFile = new CipherFile();
         CipherFile encryptFile = new CipherFile();
         try {
-            String key = "TuyMove!@#@$!"; // needs to be at least 8 characters for DES
+            String key = "TuyMove!@#@$!"; 
             FileInputStream fis2 = new FileInputStream("core.dll");
             File tempF = File.createTempFile("javatempfile",".mytemp");
             FileOutputStream fos2 = new FileOutputStream(tempF);
             decryptFile.decrypt(key, fis2, fos2);
-            
-            /* Encrypt File 
-            FileInputStream fis2 = new FileInputStream("bu_course.txt");
-            FileOutputStream fos2 = new FileOutputStream("encrypted1.txt");
-            encryptFile.encrypt(key, fis2, fos2);
-            */
+
             UI ui = new UI(tempF);
         } catch (Throwable e) {
             e.printStackTrace();
